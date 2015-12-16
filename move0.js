22226bb7cf7641a15fefd3ab2c9d502a309917b7
Ivancsics Nora <ivancsics.nora@freemail.hu>
2015-12-16 17:40:17 +0100

    first commit

---
+++ move0.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+function move0(pname, px, py, pmap) {
+	$("body").trigger({
+		type : "refreshmap",
+		name : pname,
+		walk : getDirection
+	});
+}
+
+function getDireciton(px, py) {
+	n = map[px, py + 1];
+	w = map[px - 1, py];
+	s = map[px, py - 1];
+	e = map[px + 1, py];
+
+	if (n === 1)
+		return 0;
+	if (w === 1)
+		return 1;
+	if (s === 1)
+		return 2;
+	if (e === 1)
+		return 3;
+
+	return 1;
+}
