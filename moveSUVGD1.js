dd62afdda4ff1c118ccde6d25d53b05b8fd1620b
Ivancsics Nora <ivancsics.nora@freemail.hu>
2015-12-16 22:41:26 +0100

    go north

--- moveSUVGD1.js
+++ moveSUVGD1.js
@@ -12,16 +12,18 @@ function getDirection(px, py) {
 	var west = map[px - 1][py];
 	var east = map[px + 1][py];
 
+	console.log("px: " + px + "py: " + py);
 	console.log("north: " + north + "west: " + west + "south: " + south
 			+ "east: " + east)
-	if (north === 1)
-		return 0;
-	if (west === 1)
-		return 1;
-	if (south === 1)
-		return 2;
-	if (east === 1)
-		return 3;
-
-	return Math.floor(Math.random() * 4);
+	// if (north == 1)
+	// return 0;
+	// if (west == 1)
+	// return 1;
+	// if (south == 1)
+	// return 2;
+	// if (east == 1)
+	// return 3;
+	//
+	// return Math.floor(Math.random() * 4);
+	return 2;
 }
